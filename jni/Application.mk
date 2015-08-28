8b493149c96622bec406b02fcf2e8ba7306b97e1
Pere Pujal i Carabantes <perepujal@gmail.com>
2015-08-28 02:44:38 +0200

    APP_PLATFORM should be equal or lower than minSdkVersion (Thanks to mvdan on #fdroid-dev)

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,5 @@
 APP_ABI      := armeabi armeabi-v7a
-APP_PLATFORM := android-19
+APP_PLATFORM := android-10
 APP_CFLAGS += -Wno-error=format-security
 APP_STL := stlport_shared
 APP_CPPFLAGS += -fexceptions
