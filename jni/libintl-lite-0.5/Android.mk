3448a6a77f7cb44896bd98838a4fe01828c48df0
Pere Pujal i Carabantes <perepujal@gmail.com>
2017-03-21 22:50:22 +0100

    libintl-lite now able to load their files directly from assets.

--- jni/libintl-lite-0.5/Android.mk
+++ jni/libintl-lite-0.5/Android.mk
@@ -6,10 +6,15 @@ LOCAL_MODULE := tuxpaint_intl
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
 		$(LOCAL_PATH)/internal
-	
-LOCAL_SRC_FILES := internal/libintl.cpp
+
+LOCAL_CFLAGS += -include $(LOCAL_PATH)"/../tp-assets-fopen/tp_android_assets_fopen.h"
+
+LOCAL_SRC_FILES := internal/libintl.cpp 
 
 
 LOCAL_EXPORT_C_INCLUDES += $(LOCAL_C_INCLUDES)
+
+LOCAL_SHARED_LIBRARIES := tp_android_assets_fopen
+
 
 include $(BUILD_SHARED_LIBRARY)
