49a143875d0e0f9f3ea565ec2c335046acb17278
Pere Pujal i Carabantes <perepujal@gmail.com>
2017-07-25 22:57:27 +0200

    x86 devices run arm code, disabling x86 build to narrow the apk, allow missing deps accounting for libz.

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,5 +1,7 @@
-APP_ABI      := armeabi armeabi-v7a x86
+APP_ABI      := armeabi armeabi-v7a
 APP_PLATFORM := android-10
 APP_CFLAGS += -Wno-error=format-security
 APP_STL := stlport_shared
 APP_CPPFLAGS += -fexceptions
+APP_ALLOW_MISSING_DEPS  :=  true
+
