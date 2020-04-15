0b118b325dd37ae8ebfb2df74ff44c82faf53368
Pere Pujal i Carabantes <perepujal@gmail.com>
2020-04-16 01:41:48 +0200

    Updates to build.gradle and Application.mk ot be able to compile updated libraries.

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,7 +1,7 @@
 APP_ABI      := armeabi armeabi-v7a
 APP_PLATFORM := android-16
 APP_CFLAGS += -Wno-error=format-security
-APP_STL := stlport_shared
+APP_STL := c++_shared
 APP_CPPFLAGS += -fexceptions
 APP_ALLOW_MISSING_DEPS  :=  true
 
