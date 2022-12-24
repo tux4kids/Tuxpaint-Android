916a52be8221ab293372773373dc364c2f2f9b05
Pere Pujal i Carabantes <perepujal@gmail.com>
2022-12-25 00:48:30 +0100

    Bugfix release of 0.9.28-3

--- app/src/main/jni/tuxpaint/Android.mk
+++ app/src/main/jni/tuxpaint/Android.mk
@@ -42,7 +42,7 @@ MY_CFLAGS:= -O0 -g -W -Wall -fno-common -ffloat-store \
 	-Wstrict-aliasing=2 \
 	-include $(LOCAL_PATH)"/../tp-assets-fopen/tp_android_assets_fopen.h"
 
-MY_VER_VERSION :=0.9.27
+MY_VER_VERSION :=0.9.28-3
 MY_VER_DATE :=$(shell date +"%Y-%m-%d")
 MY_NOSOUNDFLAG := 
 # MY_NOSOUNDFLAG := -DNOSOUND
