2c3e5b77dd77b947324c29ef942fc0734be1b60d
Pere Pujal i Carabantes <perepujal@gmail.com>
2023-10-08 19:45:49 +0200

    Removing librsvg 2.40.9. It had some bugs that made Tux Paint crashing with some files.needs to be replaced with a newer rsvg version, for now though Tux Paint will go without rsvg support.

--- app/src/main/jni/librsvg-2.40.9/librsvg-features.c
+++
@@ -1,7 +0,0 @@
-#include "rsvg.h"
-
-/* General initialization hooks */
-const guint librsvg_major_version = LIBRSVG_MAJOR_VERSION;
-const guint librsvg_minor_version = LIBRSVG_MINOR_VERSION;
-const guint librsvg_micro_version = LIBRSVG_MICRO_VERSION;
-const char librsvg_version[] = LIBRSVG_VERSION;
