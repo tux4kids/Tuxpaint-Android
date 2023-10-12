477e01b90a94f3b36af96cf2a7ca506caf8961f8
Pere Pujal i Carabantes <perepujal@gmail.com>
2023-10-12 23:29:01 +0200

    Discarding armeabi-v7a support.

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,9 +1,7 @@
-APP_ABI      := armeabi armeabi-v7a
+APP_ABI      := arm64-v8a
 APP_PLATFORM := android-16
 APP_CFLAGS += -Wno-error=format-security
 APP_STL := c++_shared
 APP_CPPFLAGS += -fexceptions
 APP_ALLOW_MISSING_DEPS  :=  true
 
-# Note: Uncomment the next line to build with ndk r14b in order to get older Androids support
-# NDK_TOOLCHAIN_VERSION := 4.9
