34fe3f156ad67f3354bce196e505a2266ecd5158
Pere Pujal i Carabantes <perepujal@gmail.com>
2025-05-27 00:19:56 +0200

    Updating stamps to 2025.05.26

--- app/src/main/jni/tuxpaint/mkzip_assets.sh
+++ app/src/main/jni/tuxpaint/mkzip_assets.sh
@@ -27,13 +27,13 @@ fi
 
 if [ $RUN = true ]
 then
-    stamps_version="2025.03.31"
+    stamps_version="2025.05.26"
     stampsversion=`echo $stamps_version|sed "s/\./-/g"`
 
     # The above sed line doesn't play fine with betas
     # https://sourceforge.net/projects/tuxpaint/files/tuxpaint-stamps/2024-10-XX-beta/tuxpaint-stamps-2024.10.16.tar.gz/download
     #https://sourceforge.net/projects/tuxpaint/files/tuxpaint-stamps/2025-04-XX-beta/tuxpaint-stamps-2025.03.31.tar.gz/download
-    stampsversion="2025-04-XX-beta"
+    #stampsversion="2025-04-XX-beta"
 
     if [ -d tmpzip ]
     then
