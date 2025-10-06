73c956827747bfbb2d70fbd4d79718b7b3ce0b67
Ruben Barkow-Kuder <github@r.z11.de>
2025-10-06 19:43:31 +0200

    Revert "feat: Remove 'Tools' label from toolbar"
    This reverts commit c74e7e5b861bb3a2b3cf312543f6284ec4e5f770.

--- app/src/main/jni/tuxpaint/src/tuxpaint.c
+++ app/src/main/jni/tuxpaint/src/tuxpaint.c
@@ -893,11 +893,10 @@ static void setup_normal_screen_layout(void)
   gd_toolopt.cols = 2;
   gd_tools.cols = 2;
 
-  /* Hide "Tools" label - give more space to tool buttons */
   r_ttools.x = 0;
   r_ttools.y = 0;
   r_ttools.w = gd_tools.cols * button_w;
-  r_ttools.h = 0;  /* No space for label */
+  r_ttools.h = 40 * button_scale;
 
   r_ttoolopt.w = gd_toolopt.cols * button_w;
   r_ttoolopt.h = 40 * button_scale;
@@ -991,7 +990,7 @@ static void setup_normal_screen_layout(void)
   gd_sfx.cols = 2;
 
   r_tools.x = 0;
-  r_tools.y = 0;  /* Start at top (no label above) */
+  r_tools.y = r_ttools.h + r_ttools.y;
   r_tools.w = gd_tools.cols * button_w;
   r_tools.h = gd_tools.rows * button_h;
 
@@ -10622,8 +10621,8 @@ static void draw_toolbar(void)
   off_y = 0;
   /* FIXME: Only allow print if we have something to print! */
 
-  /* "Tools" label rendering disabled - label removed */
-  /* draw_image_title(TITLE_TOOLS, r_ttools); */
+
+  draw_image_title(TITLE_TOOLS, r_ttools);
 
 
 
