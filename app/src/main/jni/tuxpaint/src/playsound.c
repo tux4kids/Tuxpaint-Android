a0386f664448ee56bb37f9a089ab61932ee397b2
Ruben Barkow-Kuder <github@r.z11.de>
2025-10-06 13:07:56 +0200

    WIP: fix sound togggle: add detailed sound control logging for Android debugging

--- app/src/main/jni/tuxpaint/src/playsound.c
+++ app/src/main/jni/tuxpaint/src/playsound.c
@@ -54,8 +54,15 @@ void playsound(SDL_Surface *screen, int chan, int s, int override, int x, int y)
 #ifndef NOSOUND
   int left, dist;
 
+#ifdef __ANDROID__
+  __android_log_print(ANDROID_LOG_INFO, "TuxPaint", "PLAYSOUND_LOG: Called - mute=%d, use_sound=%d, sound=%d", mute, use_sound, s);
+#endif
+  
   if (!mute && use_sound && s != SND_NONE)
   {
+#ifdef __ANDROID__
+    __android_log_print(ANDROID_LOG_INFO, "TuxPaint", "PLAYSOUND_LOG: Condition passed - playing sound %d on channel %d", s, chan);
+#endif
 #ifdef DEBUG
     printf("playsound #%d in channel %d, pos (%d,%d), %soverride, ptr=%p\n",
            s, chan, x, y, override ? "" : "no ", sounds[s]);
@@ -108,11 +115,16 @@ void playsound(SDL_Surface *screen, int chan, int s, int override, int x, int y)
         left = (255 - dist) / 2;
       }
 #ifdef DEBUG
-      printf("Panning of sound #%d in channel %d, left=%d, right=%d\n", s, chan, left, (255 - dist) - left);
       fflush(stdout);
 #endif
       Mix_SetPanning(chan, left, (255 - dist) - left);
     }
+  }
+  else
+  {
+#ifdef __ANDROID__
+    __android_log_print(ANDROID_LOG_INFO, "TuxPaint", "PLAYSOUND_LOG: Sound blocked - mute=%d, use_sound=%d, sound=%d", mute, use_sound, s);
+#endif
   }
 #endif
 }
